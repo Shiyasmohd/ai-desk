5fc3eff9b4941963192231888452ceb86b4080c0
shiyasmohd <shiyasjaseena124@gmail.com>
2023-02-11 23:42:53 +0530

    new ai added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,15 @@ import AnimeAIImg from '../../public/animeai.png'
 import RunwayImg from '../../public/runway.png'
 import NamelixImg from '../../public/namelix.png'
 import LookaImg from '../../public/looka.png'
+import PoisedImg from '../../public/poised.png'
+import TripPlannerImg from '../../public/trip-planner.png'
+import SupermemeImg from '../../public/supermeme.png'
+import AlteredImg from '../../public/altered.png'
+import MemImg from '../../public/mem.png'
+import LalalImg from '../../public/lalal.png'
+import DurableImg from '../../public/durable.png'
+import QuillbotImg from '../../public/quillbot.png'
+import ElevenlabsImg from '../../public/elevenlabs.png'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './Tabs'
 import Footer from './footer'
 
@@ -79,11 +88,32 @@ export default function Home() {
       category: "productive"
     },
     {
+      name: "Durable",
+      caption: "Build a website in 30 seconds",
+      image: DurableImg,
+      link: "https://durable.co/",
+      category: "productive"
+    },
+    {
+      name: "Altered",
+      caption: "Augment Your Voice.",
+      image: AlteredImg,
+      link: "https://www.altered.ai/",
+      category: "productive"
+    },
+    {
       name: "Looka",
       caption: "Create logos in minutes.",
       image: LookaImg,
       link: "https://looka.com/",
       category: "creative"
+    },
+    {
+      name: "Mem",
+      caption: "Self-organizing workspace using AI",
+      image: MemImg,
+      link: "https://get.mem.ai/",
+      category: "productive"
     },
     {
       name: "Namelix",
@@ -100,6 +130,20 @@ export default function Home() {
       category: "creative"
     },
     {
+      name: "Trip Planner",
+      caption: "Plan your trip using AI. ",
+      image: TripPlannerImg,
+      link: "https://www.buildai.space/app/dae3da25-888e-448f-b15c-5a20ca4ca961",
+      category: "productive"
+    },
+    {
+      name: "Eleven Labs",
+      caption: "Text to Voice",
+      image: ElevenlabsImg,
+      link: "https://beta.elevenlabs.io/",
+      category: "productive"
+    },
+    {
       name: "Fliki",
       caption: "Create reels using AI.",
       image: FlikiImg,
@@ -114,12 +158,40 @@ export default function Home() {
       category: "productive"
     },
     {
+      name: "Supermeme",
+      caption: "Create memes with the power of AI.",
+      image: SupermemeImg,
+      link: "https://www.supermeme.ai/",
+      category: "creative"
+    },
+    {
+      name: "Poised",
+      caption: "Your AI communication coach.",
+      image: PoisedImg,
+      link: "https://www.poised.com/",
+      category: "productive"
+    },
+    {
+      name: "Lalal",
+      caption: "Seperate audio tracks using AI. ",
+      image: LalalImg,
+      link: "https://www.lalal.ai/",
+      category: "productive"
+    },
+    {
       name: "Anime AI",
       caption: "Anime AI generator.",
       image: AnimeAIImg,
       link: "https://www.zmo.ai/ai-anime-generator/",
       category: "creative"
-    }
+    },
+    {
+      name: "Quillbot",
+      caption: "AI-powered paraphrasing tool.",
+      image: QuillbotImg,
+      link: "https://quillbot.com/",
+      category: "productive"
+    },
   ]
 
   return (
